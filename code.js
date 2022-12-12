02675584f2d2101002d209015fefa28815e6987b
woooowoooo <51186839+woooowoooo@users.noreply.github.com>
2022-12-12 02:52:44 -0800

    Remove state machine
    This tool will be a lot easier to manage state-wise than wooootris because it won't have a lot of dialogs and menus, so I won't need this library anymore

--- code.js
+++ code.js
@@ -214,160 +214,118 @@ async function loadResources() {
 	}
 	return Promise.all(promises);
 }
-// State machine
-const stateMachine = new StateMachine({
-	init: "boot",
-	transitions: [
-		{
-			name: "toMenu",
-			from: "*",
-			to: "menu"
-		},
-		{
-			name: "start",
-			from: "menu",
-			to: "main"
-		},
-		{
-			name: "toSettings",
-			from: "menu",
-			to: "settings"
-		},
-		{
-			name: "toCredits",
-			from: "menu",
-			to: "credits"
-		},
-		{
-			name: "lose",
-			from: "main",
-			to: "gameOver"
-		},
-		{
-			name: "retry",
-			from: "gameOver",
-			to: "main"
+// Boot
+context.fillStyle = "black";
+context.fillRect(0, 0, 1920, 1280);
+context.fillStyle = "white";
+context.fontSize = 16;
+context.textAlign = "center";
+context.fillText("LOADING", 960, 400);
+context.fontSize = 8;
+context.fillText("If this doesn't go away,", 960, 800);
+context.fillText("refresh the page.", 960, 960);
+await loadResources();
+console.log("Resources loaded.", images, sounds);
+// Prompt for user interaction so autoplay won't get blocked
+clear();
+context.fillStyle = "black";
+context.fillRect(0, 0, 1920, 1280);
+context.fillStyle = "white";
+context.fontSize = 8;
+context.fillText("Loading finished.", 960, 400);
+context.fillText("CLICK ANYWHERE", 960, 800);
+context.fillText("TO CONTINUE", 960, 960);
+wrapClickEvent(onMenu);
+// State transitions
+function onMenu() {
+	clear();
+	sounds.mainTheme.play();
+	objects.set("background", new Drawable(() => context.drawImage(images.background, 0, 0, 1920, 1280)));
+	objects.set("title", new Drawable(() => {
+		context.fillStyle = "white";
+		context.fontSize = 20;
+		context.fillText("wooootris", 960, 320);
+	}));
+	objects.set("start", new TextButton(960, 520, "Start", () => stateMachine.start("default"), 640));
+	objects.set("fortyLines", new TextButton(960, 680, "40 Lines", () => stateMachine.start("fortyLines"), 640));
+	objects.set("settings", new TextButton(960, 840, "Settings", stateMachine.toSettings, 640));
+	objects.set("credits", new TextButton(960, 1000, "Credits", stateMachine.toCredits, 640));
+	objects.set("mute", new MuteButton());
+};
+function onSettings() {
+	clear();
+	objects.set("background", new Drawable(() => context.drawImage(images.background, 0, 0, 1920, 1280)));
+	objects.set("text", new Drawable(() => {
+		context.fillStyle = "white";
+		context.textAlign = "right";
+		context.fillText("Grid:", 600, 280 - 20 + 28);
+		context.fillText("ARR:", 600, 440 + 28);
+		context.fillText("DAS:", 600, 600 + 28);
+		context.fillText("Volume:", 600, 760 + 28);
+	}));
+	objects.set("grid", new TextToggle(1200, 280 - 20 + 28 - 92, "grid"));
+	objects.set("arr", new Slider(1200, 440, 960, "arr", 0, 5));
+	objects.set("das", new Slider(1200, 600, 960, "das", 0, 20));
+	objects.set("volume", new Slider(1200, 760, 960, "volume", 0, 100, 10, false, () => {
+		for (const sound of Object.values(sounds)) {
+			sound.volume = settings.volume / 100;
 		}
-	],
-	methods: {
-		onTransition(lifecycle) {
-			console.log(`Transition: ${lifecycle.transition}\tNew State: ${lifecycle.to}`);
-		},
-		async onBoot() {
-			// Loading screen
-			context.fillStyle = "black";
-			context.fillRect(0, 0, 1920, 1280);
-			context.fillStyle = "white";
-			context.fontSize = 16;
-			context.textAlign = "center";
-			context.fillText("LOADING", 960, 400);
-			context.fontSize = 8;
-			context.fillText("If this doesn't go away,", 960, 800);
-			context.fillText("refresh the page.", 960, 960);
-			await loadResources();
-			console.log("Resources loaded.", images, sounds);
-			// Prompt for user interaction so autoplay won't get blocked
-			clear();
-			context.fillStyle = "black";
-			context.fillRect(0, 0, 1920, 1280);
-			context.fillStyle = "white";
-			context.fontSize = 8;
-			context.fillText("Loading finished.", 960, 400);
-			context.fillText("CLICK ANYWHERE", 960, 800);
-			context.fillText("TO CONTINUE", 960, 960);
-			wrapClickEvent(stateMachine.toMenu);
-		},
-		onMenu() {
-			clear();
-			sounds.mainTheme.play();
-			objects.set("background", new Drawable(() => context.drawImage(images.background, 0, 0, 1920, 1280)));
-			objects.set("title", new Drawable(() => {
-				context.fillStyle = "white";
-				context.fontSize = 20;
-				context.fillText("wooootris", 960, 320);
-			}));
-			objects.set("start", new TextButton(960, 520, "Start", () => stateMachine.start("default"), 640));
-			objects.set("fortyLines", new TextButton(960, 680, "40 Lines", () => stateMachine.start("fortyLines"), 640));
-			objects.set("settings", new TextButton(960, 840, "Settings", stateMachine.toSettings, 640));
-			objects.set("credits", new TextButton(960, 1000, "Credits", stateMachine.toCredits, 640));
-			objects.set("mute", new MuteButton());
-		},
-		onSettings() {
-			clear();
-			objects.set("background", new Drawable(() => context.drawImage(images.background, 0, 0, 1920, 1280)));
-			objects.set("text", new Drawable(() => {
-				context.fillStyle = "white";
-				context.textAlign = "right";
-				context.fillText("Grid:", 600, 280 - 20 + 28);
-				context.fillText("ARR:", 600, 440 + 28);
-				context.fillText("DAS:", 600, 600 + 28);
-				context.fillText("Volume:", 600, 760 + 28);
-			}));
-			objects.set("grid", new TextToggle(1200, 280 - 20 + 28 - 92, "grid"));
-			objects.set("arr", new Slider(1200, 440, 960, "arr", 0, 5));
-			objects.set("das", new Slider(1200, 600, 960, "das", 0, 20));
-			objects.set("volume", new Slider(1200, 760, 960, "volume", 0, 100, 10, false, () => {
-				for (const sound of Object.values(sounds)) {
-					sound.volume = settings.volume / 100;
-				}
-			}));
-			objects.set("return", new TextButton(960, 1000, "Return", stateMachine.toMenu, 640));
-			objects.set("mute", new MuteButton());
-		},
-		onCredits() {
-			clear();
-			objects.set("background", new Drawable(() => context.drawImage(images.background, 0, 0, 1920, 1280)));
-			objects.set("credits", new Drawable(() => {
-				context.fillStyle = "white";
-				context.fontSize = 8;
-				context.fillText("Everything", 960, 360);
-				context.fillText("woooowoooo", 960, 440);
-				context.fillText("Inspiration and testing", 960, 640);
-				context.fillText("yayuuuhhhh", 960, 720);
-			}));
-			objects.set("return", new TextButton(960, 1000, "Return", stateMachine.toMenu, 640));
-			objects.set("mute", new MuteButton());
-		},
-		onMain(_, newMode = mode) {
-			mode = newMode;
-			clear();
-			newGame(mode, settings);
-			window.addEventListener("keydown", onKeyDown);
-			window.addEventListener("keyup", onKeyUp);
-			objects.set("background", new Drawable(() => context.drawImage(images.background, 0, 0, 1920, 1280)));
-			objects.set("mute", new MuteButton());
-			objects.set("tetris", new Drawable(() => tetrisRender(context)));
-			requestAnimationFrame(loop);
-		},
-		onGameOver(_, text) {
-			window.removeEventListener("keydown", onKeyDown);
-			window.removeEventListener("keyup", onKeyUp);
-			paused = true;
-			for (const sound of Object.values(sounds).filter(sound => !sound.paused)) {
-				sound.pause();
-			}
-			objects.set("endScreen", new Drawable(() => {
-				context.fillStyle = "rgba(0, 0, 0, 0.5)";
-				context.fillRect(0, 0, 1920, 1280);
-				context.fillStyle = "white";
-				context.fontSize = 16;
-				context.textAlign = "center";
-				context.fillText("GAME OVER", 960, 400);
-				context.fontSize = 8;
-				let textY = 540;
-				for (const line of text) {
-					context.fillText(line, 960, textY);
-					textY += 100;
-				}
-			}));
-			objects.set("menu", new TextButton(672, 920, "Menu", stateMachine.toMenu, 480, true));
-			objects.set("retry", new TextButton(1248, 920, "Retry", stateMachine.retry, 480, true));
-		},
-		onLeaveGameOver() {
-			paused = false;
-			for (const sound of Object.values(sounds).filter(sound => sound.paused)) {
-				sound.play();
-			}
-		}
+	}));
+	objects.set("return", new TextButton(960, 1000, "Return", stateMachine.toMenu, 640));
+	objects.set("mute", new MuteButton());
+};
+function onCredits() {
+	clear();
+	objects.set("background", new Drawable(() => context.drawImage(images.background, 0, 0, 1920, 1280)));
+	objects.set("credits", new Drawable(() => {
+		context.fillStyle = "white";
+		context.fontSize = 8;
+		context.fillText("Everything", 960, 360);
+		context.fillText("woooowoooo", 960, 440);
+		context.fillText("Inspiration and testing", 960, 640);
+		context.fillText("yayuuuhhhh", 960, 720);
+	}));
+	objects.set("return", new TextButton(960, 1000, "Return", stateMachine.toMenu, 640));
+	objects.set("mute", new MuteButton());
+};
+function onMain(_, newMode = mode) {
+	mode = newMode;
+	clear();
+	newGame(mode, settings);
+	window.addEventListener("keydown", onKeyDown);
+	window.addEventListener("keyup", onKeyUp);
+	objects.set("background", new Drawable(() => context.drawImage(images.background, 0, 0, 1920, 1280)));
+	objects.set("mute", new MuteButton());
+	objects.set("tetris", new Drawable(() => tetrisRender(context)));
+	requestAnimationFrame(loop);
+};
+function onGameOver(_, text) {
+	window.removeEventListener("keydown", onKeyDown);
+	window.removeEventListener("keyup", onKeyUp);
+	paused = true;
+	for (const sound of Object.values(sounds).filter(sound => !sound.paused)) {
+		sound.pause();
 	}
-});
+	objects.set("endScreen", new Drawable(() => {
+		context.fillStyle = "rgba(0, 0, 0, 0.5)";
+		context.fillRect(0, 0, 1920, 1280);
+		context.fillStyle = "white";
+		context.fontSize = 16;
+		context.textAlign = "center";
+		context.fillText("GAME OVER", 960, 400);
+		context.fontSize = 8;
+		let textY = 540;
+		for (const line of text) {
+			context.fillText(line, 960, textY);
+			textY += 100;
+		}
+	}));
+	objects.set("menu", new TextButton(672, 920, "Menu", stateMachine.toMenu, 480, true));
+	objects.set("retry", new TextButton(1248, 920, "Retry", stateMachine.retry, 480, true));
+};
+function onLeaveGameOver() {
+	paused = false;
+	for (const sound of Object.values(sounds).filter(sound => sound.paused)) {
+		sound.play();
+	}
+};
